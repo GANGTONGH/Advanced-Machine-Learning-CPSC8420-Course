584292fea084bda330c109840475cfce29adb30d
__HGTGH__ <155127242+GANGTONGH@users.noreply.github.com>
2024-12-29 16:23:45 -0500

    Update nn-statis.py

--- Final Project/CA_NN_2_new_upload/nn-statis.py
+++ Final Project/CA_NN_2_new_upload/nn-statis.py
@@ -13,8 +13,11 @@ def usage():
 # Decide format of well potential from CA_NN_info
 # based on possible SSE
 def read_ca_nn_info(in_filename):
+    # Residue list
     map_list = []
+    # Dist bins
     range_list = []
+    # Number of bins
     steps_list = []
     with open(in_filename, 'r') as in_file:
         for line in in_file:
@@ -46,12 +49,13 @@ def read_ca_nn_data(in_filename, cut, map_list, steps_list):
                 if value < cut[i1][i]:
                     irange = i
                     break
+            # If dist is too big, clasiffy into rightmost bin.
             if irange < 0 and value < 7.66:
                 irange = steps_list[i1] // 2 - 1
 
             if irange >= 0:
                 statis[i0][i1][i2][irange] += 1
-                res_nrec[i1] += 1
+                res_nrec[i1] += 1 # Number of records for resi i1
     return statis, res_nrec
 
 def perform_bayesian_statistics(statis, res_nrec, steps_list, map_list):
@@ -59,8 +63,8 @@ def perform_bayesian_statistics(statis, res_nrec, steps_list, map_list):
         states = steps_list[c] // 2
         print(states)
         if res_nrec[c] > 0:
-            PN_S = np.zeros((20, 20, states))
-            # PN_S = np.zeros((20, 20, 3))
+            PN_S = np.zeros((20, 20, states)) # 
+            # Smoothing prob of statis[p][c][n]
             for p in range(len(map_list)):
                 for n in range(len(map_list)):
                     tmp_sum = np.sum(statis[p][c][n])
@@ -68,6 +72,7 @@ def perform_bayesian_statistics(statis, res_nrec, steps_list, map_list):
                         tmp_ps = np.zeros(states)
                         tmp_ns = np.zeros(states)
                         nnn = 0
+                        # Add all i-1,i overlapping sequence probability
                         for nn in range(len(map_list)):
                             if nn != n:
                                 ss_sum = np.sum(statis[p][c][nn])
@@ -77,6 +82,7 @@ def perform_bayesian_statistics(statis, res_nrec, steps_list, map_list):
                         if nnn > 0:
                             tmp_ns /= nnn
                         nnn = 0
+                        # Add all i,i+1 overlapping sequence probability
                         for pp in range(len(map_list)):
                             if pp != p:
                                 ss_sum = np.sum(statis[pp][c][n])
@@ -85,6 +91,7 @@ def perform_bayesian_statistics(statis, res_nrec, steps_list, map_list):
                                     tmp_ps += statis[pp][c][n] / ss_sum
                         if nnn > 0:
                             tmp_ps /= nnn
+                        # P(i-1,i,i+1) ~= Sum(x, P(i-1,i,x)) * Sum(x, P(x,i,i+1))
                         tmp_sum = np.sum(tmp_ns * tmp_ps)
                         if tmp_sum > 0:
                             PN_S[p][n] = tmp_ns * tmp_ps
@@ -102,10 +109,10 @@ def perform_bayesian_statistics(statis, res_nrec, steps_list, map_list):
             B = np.zeros(M)
             index = 0
 
-            for i in range(20):
-                for j in range(20):
-                    for k in range(states):
-                        B[index] = PN_S[i][j][k]
+            for i in range(20): # p
+                for j in range(20): # n
+                    for k in range(states): # Type of SS
+                        B[index] = PN_S[i][j][k] # probability of triplet sequence (p, *c, n) given SS 
                         p_s = i * states + k
                         n_s = j * states + k + N_HALF
                         A[index][p_s] = 1
@@ -123,8 +130,8 @@ def perform_bayesian_statistics(statis, res_nrec, steps_list, map_list):
             subprocess.run([SVDSolver, str(M), str(N), Amatrix, Bvector, "0.00001"], check=True)
 
             # Read the SVD data
-            PSI_AA = np.zeros((20, states))
-            PHI_AA = np.zeros((20, states))
+            PSI_AA = np.zeros((20, states)) # Prior: P( i-1 | i, i+1, state)
+            PHI_AA = np.zeros((20, states)) # Prior: P( i+1 | i-1, i, state)
             with open(Xsol, 'r') as f:
                 next(f)  # Skip the first line
                 iline = 0
@@ -151,27 +158,30 @@ def perform_bayesian_statistics(statis, res_nrec, steps_list, map_list):
             # Obtain Xi and expectation values
             K = 0.5
             alpha = np.zeros((20, 20, states))
-            exp = np.zeros((20, 20, states))
-            exp_counts = np.zeros((20, 20, states))
+            expectation = np.zeros((20, 20, states))
+            expectation_counts = np.zeros((20, 20, states))
 
             for i in range(20):
                 for j in range(20):
                     for iaa in range(states):
+                        # Joint prior prob P(i-1, i, i+1) = PSI * PHI
                         alpha[i][j][iaa] = PSI_AA[i][iaa] * PHI_AA[j][iaa]
                     Y0 = np.sum(statis[i][c][j])
                     A0 = np.sum(alpha[i][j])
                     OCCUP = np.clip(Y0 * K, states, states * 5)
                     scale = OCCUP / A0
                     alpha[i][j] *= scale
-                    exp[i][j] = (alpha[i][j] + statis[i][c][j][:states] + 1) / (OCCUP + Y0 + states)
+                    expectation[i][j] = (alpha[i][j] + statis[i][c][j][:states] + 1) / (OCCUP + Y0 + states)
+                    # With NO observation, P(triplet, alpha) = P(triplet, beta) = P(triplet, PPII) = 1/3 or 1/2
                     Y0 = max(Y0, 1)
-                    exp_counts[i][j] = Y0 * (alpha[i][j] + statis[i][c][j][:states] + 1) / (OCCUP + Y0 + states)
-
+                    expectation_counts[i][j] = Y0 * (alpha[i][j] + statis[i][c][j][:states] + 1) / (OCCUP + Y0 + states)
+                    # Scaled counts
             # Output posterior data
             for i in range(20):
                 for j in range(20):
                     for k in range(states):
-                        print(f"{map_list[i]} {map_list[c]} {map_list[j]} {k:4d}: {statis[i][c][j][k]:4d} {exp_counts[i][j][k]:8.3f} {exp[i][j][k]:8.3f}")
+                        print(f"{map_list[i]} {map_list[c]} {map_list[j]} {k:4d}: {statis[i][c][j][k]:4d} {expectation_counts[i][j][k]:8.3f} {expectation[i][j][k]:8.3f}")
+                    
 
 def main():
     if len(sys.argv) < 3:
